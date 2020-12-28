2f7d3124a63982037412f39925f5f173f8b8983b
Bob <bob_w@qq.com>
2020-12-28 17:11:47 +0800

    使用装饰器重构跨层传值

--- src/components/ContextSample.js
+++ src/components/ContextSample.js
@@ -9,18 +9,41 @@ const store = {
   }
 }
 
-export default class ContextSample extends Component {
+const withProvider = Comp => (
+  props => (
+    <Context.Provider value={store}>
+      <Comp {...props}></Comp>
+    </Context.Provider>
+  )
+)
+
+const withConsumer = Comp => (
+  props => (
+    <Context.Consumer>
+      {/* 必须内嵌一个函数 */}
+      {val => (<Comp {...props} onClick={() => val.sayName()} value={val.name}></Comp>)}
+    </Context.Consumer>
+  )
+)
+
+@withConsumer
+class Inner extends Component {
   render() {
     return (
-      <Context.Provider value={store}>
-        <div>
-          {/* 获取数据 */}
-          <Context.Consumer>
-            {/* 必须内嵌一个函数 */}
-            {val => (<button onClick={() => val.sayName()}>{val.name}</button>)}
-          </Context.Consumer>
-        </div>
-      </Context.Provider>
+      <button onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+@withProvider
+class ContextSample extends Component {
+  render() {
+    return (
+      <div><Inner></Inner></div>
     )
   }
 }
+
+export default ContextSample
