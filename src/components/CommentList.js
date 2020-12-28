a3dcb22d98b75c2b9189eccc88678d33e61fa8ea
Bob <bob_w@qq.com>
2020-12-28 11:21:06 +0800

    使用shouldComponentUpdate进行渲染的优化

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 
-function Comment({ data }) {
-  return (
-    <div>
-      <p>-{data.author}-</p>
-      <p>{data.body}</p>
-    </div>
-  )
+// function Comment({ data }) {
+//   console.log('render');
+//   return (
+//     <div>
+//       <p>-{data.author}-</p>
+//       <p>{data.body}</p>
+//     </div>
+//   )
+// }
+class Comment extends Component {
+
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.data.author === this.props.data.author && nextProps.data.body === this.props.data.body) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <p>-{this.props.data.author}-</p>
+        <p>{this.props.data.body}</p>
+      </div>
+    )
+  }
 }
+
 
 
 export default class CommentList extends Component {
@@ -20,7 +41,7 @@ export default class CommentList extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
+    setInterval(() => {
       this.setState({
         comments: [
           { body: 'How are you?', author: 'Bob' },
