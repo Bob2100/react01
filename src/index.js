da0459c5beff6703c4fdb68812b516f637fbcbd5
Bob <bob_w@qq.com>
2020-12-30 15:02:48 +0800

    react-redux的使用

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// import CartSample from './CartSample';
-// import App from './App';
-// import Lifecycle from "./Lifecycle";
-// import CommentList from "./components/CommentList";
-// import WelcomeDialog from './components/Composition'
-import Hoc from './components/Hoc'
+import ReduxTest from './components/ReduxTest';
+import store from "./store";
+import { Provider } from "react-redux";
 
-// ReactDom.render(<h1>React真酷</h1>, document.querySelector('#root'));
-// ReactDom.render(<App />, document.querySelector('#root'));
-// ReactDom.render(<Lifecycle />, document.querySelector('#root'));
-// ReactDom.render(<CartSample title="购物车案例" />, document.querySelector('#root'));
-// ReactDom.render(<CommentList />, document.querySelector('#root'));
-// ReactDom.render(<WelcomeDialog />, document.querySelector('#root'));
-ReactDom.render(<Hoc stage={'入门阶段'} />, document.querySelector('#root'));
+ReactDom.render(
+  <Provider store={store}>
+    <ReduxTest />
+  </Provider>
+  , document.querySelector('#root'));
