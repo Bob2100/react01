9ca5663a29091096a6c367bac57dfae29f298ebd
Bob <bob_w@qq.com>
2021-01-05 20:39:25 +0800

    Switch从路由中选一个

--- src/components/RouterSample.js
+++ src/components/RouterSample.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Link, Route } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div>
+      <p>App 导航</p>
       <ul>
         <li>
           <Link to="/">home</Link>
@@ -16,17 +17,26 @@ function App() {
         </li>
       </ul>
       {/* 路由配置 */}
-      <Route exact path="/" component={Home}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/detail/:course" component={Detail}></Route>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/detail/:course" component={Detail}></Route>
+        <Route component={NoMatch}></Route>
+      </Switch>
     </div>
   );
 }
 
+function NoMatch() {
+  return (
+    <div>404</div>
+  );
+}
+
 function Home({ location }) {
-  console.log(location.state);
   return (
     <div>
+      <p>Home Page</p>
       <ul>
         <li><Link to="/detail/web">Web</Link></li>
         <li><Link to="/detail/java">Java</Link></li>
@@ -39,6 +49,7 @@ function Home({ location }) {
 function Detail({ match, history, location }) {
   return (
     <div>
+      <p>Detail Page</p>
       {match.params.course}
       <button onClick={history.goBack}>后退</button>
       <button onClick={() => history.push({ pathname: '/', state: { foo: 'bar' } })}>回到首页</button>
