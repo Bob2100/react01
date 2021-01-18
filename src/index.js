e922f7fa2d5b9ecae961e8a4d5184c5aeefe224b
Bob <bob_w@qq.com>
2021-01-18 13:59:39 +0800

    react原理-虚拟dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import RouterSample from './components/RouterSample'
 
-ReactDom.render(<RouterSample />, document.querySelector('#root'));
+function Comp(props) {
+  return <h2>Hi,{props.name}</h2>
+}
+
+const jsx = (
+  <div id="demo">
+    <span>Hi</span>
+    <Comp name="kaikeba"></Comp>
+  </div>
+);
+console.log(jsx);
+
+ReactDom.render(jsx, document.querySelector('#root'));
